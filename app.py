20a0534f811cb0fdaf3fe487a98c353fe2ed5ccb
duvi1234 <durgapc35@gmail.com>
2025-07-28 10:41:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,4 @@ def generate():
 def download():
     return send_from_directory("static", "final_video.mp4", as_attachment=True)
 
-if __name__ == "__main__":
-    app.run(debug=True)
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if not set
-    app.run(host="0.0.0.0", port=port, debug=True)
-
     
